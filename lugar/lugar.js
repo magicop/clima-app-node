b7aada1b6f50b8a6247ef27ffa856b333808d7a2
Magico <magicocarella@gmail.com>
2020-05-11 04:04:37 +0100

    Primer commit

---
+++ lugar/lugar.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+
+    const encodedUrl = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodedUrl }`,
+        headers: { 'x-rapidapi-key': '8e49b028e2msh2382a494e645e11p124dbbjsn944e24f4b480' }
+    });
+
+    // código 200 se realizó correctamente
+    // código 404 no se encontró la url
+    // código 500 error con el servidor
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${ dir }`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+
+}
+
+module.exports = {
+    getLugarLatLng
+}
